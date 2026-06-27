e8843f8bf908d10676a4f5b7bbe7ebf877a64a37
Yousof_RD <ysfiran@gamil.com>
2026-06-27 12:43:45 +0330

    WallSideState completed

--- core/src/main/java/Yousof/HollowKnight/Model/entities/knight/state/WallSideState.java
+++ core/src/main/java/Yousof/HollowKnight/Model/entities/knight/state/WallSideState.java
@@ -1,11 +1,14 @@
 package Yousof.HollowKnight.Model.entities.knight.state;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import Yousof.HollowKnight.Enum.Constants;
+import Yousof.HollowKnight.Enum.Keys;
 import Yousof.HollowKnight.Enum.Animations.Animations;
 import Yousof.HollowKnight.Model.entities.knight.Knight;
 
@@ -15,15 +18,49 @@ public class WallSideState extends IKnightState{
     @Override
     public void enter(Knight knight) {  
         super.enter(knight);
-        animation = Animations.Knight.create("Wall Side", PlayMode.LOOP, 0.08f);
+        animation = Animations.Knight.create("Wall Slide", PlayMode.LOOP, 0.08f);
     }
 
     @Override
     public void update(float delta) {
         super.update(delta);
 
+        if(knight.getSurroundSensors().downSensor > 0){
+            knight.changeState(new IdleState());
+            return;
+        }
 
-        
+        if(Gdx.input.isKeyPressed(Keys.KNIGHTRIGHT.key) && knight.getSurroundSensors().rightSensor > 0){
+            body.setLinearVelocity( 0f , body.getLinearVelocity().y * 0.8f);
+            return;
+        }
+
+        if(Gdx.input.isKeyPressed(Keys.KNIGHTLEFT.key) && knight.getSurroundSensors().leftSensor > 0){
+            body.setLinearVelocity( 0f , body.getLinearVelocity().y * 0.8f);
+            return;
+        }
+
+        if(Gdx.input.isKeyPressed(Keys.KNIGHTJUMP.key) && knight.getSurroundSensors().rightSensor > 0){
+            body.applyLinearImpulse(new Vector2(-1f , 1f), body.getPosition(), true);
+            return;
+        }
+
+        if(Gdx.input.isKeyPressed(Keys.KNIGHTJUMP.key) && knight.getSurroundSensors().leftSensor > 0){
+            body.applyLinearImpulse(new Vector2(1f , 1f), body.getPosition(), true);
+            return;
+        }
+
+        if(Gdx.input.isKeyPressed(Keys.KNIGHTRIGHT.key) && knight.getSurroundSensors().leftSensor > 0){
+            body.applyLinearImpulse(new Vector2(1f , 0f), body.getPosition(), true);
+            return;
+        }
+
+        if(Gdx.input.isKeyPressed(Keys.KNIGHTLEFT.key) && knight.getSurroundSensors().rightSensor > 0){
+            body.applyLinearImpulse(new Vector2(-1f , 0f), body.getPosition(), true);
+            return;
+        }
+
+        knight.changeState(new FallState());
     }
 
     @Override
